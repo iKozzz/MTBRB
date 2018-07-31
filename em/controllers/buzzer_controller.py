0b261148c87bf581da3ad2b57118d27405a50ad3
ikozzz <minskcitizen@gmail.com>
2018-07-31 21:11:30 +0300

    play with buzzer

--- em/controllers/buzzer_controller.py
+++ em/controllers/buzzer_controller.py
@@ -4,17 +4,15 @@ bz = Buzzer(10)
 
 
 def server_startup_alert():
-    bz.blink(on_time=1, off_time=0.5, n=3, background=True)
+    bz.blink(on_time=0.5, off_time=0.5, n=3, background=True)
 
 
 def ride_start_alert():
-    bz.on()
-    # bz.blink(on_time=1, off_time=0.5, n=3, background=True)
+    bz.blink(on_time=3, off_time=0.5, n=1, background=True)
 
 
 def ride_finish_alert():
-    bz.off()
-    # bz.blink(on_time=1, off_time=0.5, n=3, background=True)
+    bz.blink(on_time=2, off_time=0.5, n=2, background=True)
 
 
 def ride_cancel_alert():
--- em/urls.py
+++ em/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
+from em.controllers.buzzer_controller import server_startup_alert
 from . import views
 
 app_name = 'em'
@@ -37,3 +38,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+server_startup_alert()

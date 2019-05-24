af1e7b42778fee89583cddc05fbb6b2e61f3f03b
Konstantin Alesionok <minskcitizen@gmail.com>
2019-05-24 13:37:04 +0300

    her

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IP="$(hostname -I | cut -d' ' -f1):80"
-bash -c "nohup sudo python manage.py runserver $IP &>/dev/null &"
-echo "Application should start in a seconds! Use $IP to access server"
